898f51af202efabbc0463962c3858dbff976d9f7
Vito Farenga <vitofarenga@libero.it>
2022-06-28 17:15:57 +0200

    ultimato e funzionante

--- main.js
+++ main.js
@@ -1,31 +1,31 @@
 const team = [
     {
-       immagine : "wayne-barnett-founder-ceo.jpg",
+       immagine : "img/wayne-barnett-founder-ceo.jpg",
        nome : "Wayne Barnett",
        ruolo : "Founder & CEO",
     },
     {
-        immagine : "angela-caroll-chief-editor.jpg",
+        immagine : "img/angela-caroll-chief-editor.jpg",
         nome : "Angela Carroll",
         ruolo : "Chief Editor",
      },
      {
-        immagine : "walter-gordon-office-manager.jpg",
+        immagine : "img/walter-gordon-office-manager.jpg",
         nome : "Walter Gordon",
         ruolo : "Office Manager",
      },
      {
-        immagine : "angela-lopez-social-media-manager.jpg",
+        immagine : "img/angela-lopez-social-media-manager.jpg",
         nome : "Angela Lpoez",
         ruolo : "Social Media Manager",
      },
      {
-         immagine : "scott-estrada-developer.jpg", 
+         immagine : "img/scott-estrada-developer.jpg", 
          nome : "Scott Estrada",
          ruolo : "Developer",
       },
       {
-         immagine : "barbara-ramos-graphic-designer.jpg",
+         immagine : "img/barbara-ramos-graphic-designer.jpg",
          nome : "Barbara Ramos",
          ruolo : "Graphic Designer",
       },
@@ -34,7 +34,7 @@ const team = [
 const content = document.getElementById('carte')
 
 for(i = 0; i < team.length; i++){
-    content.innerHTML += `<div>
+    content.innerHTML += `<div class=cards>
     <img src="${team[i].immagine}">
     <span>${team[i].nome}</span>
     <span>${team[i].ruolo}</span>
