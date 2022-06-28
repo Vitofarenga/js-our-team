e48cfe3604e29fe71050e26008e0c8f6dd21025f
Vito Farenga <vitofarenga@libero.it>
2022-06-28 15:59:26 +0200

    array finito

---
+++ main.js
@@ -0,0 +1,32 @@
+const team = [
+    {
+       immagine : "wayne-barnett-founder-ceo.jpg",
+       nome : "Wayne Barnett",
+       ruolo : "Founder & CEO"
+    },
+    {
+        immagine : "angela-caroll-chief-editor.jpg",
+        nome : "Angela Carroll",
+        ruolo : "Chief Editor"
+     },
+     {
+        immagine : "walter-gordon-office-manager.jpg",
+        nome : "Walter Gordon",
+        ruolo : "Office Manager"
+     },
+     {
+        immagine : "angela-lopez-social-media-manager.jpg",
+        nome : "Angela Lpoez",
+        ruolo : "Social Media Manager"
+     },
+     {
+         immagine : "scott-estrada-developer.jpg",
+         nome : "Scott Estrada",
+         ruolo : "Developer"
+      },
+      {
+         immagine : "barbara-ramos-graphic-designer.jpg",
+         nome : "Barbara Ramos",
+         ruolo : "Graphic Designer"
+      },
+];
