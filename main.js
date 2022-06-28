b16b44ac01b6890db73a07657c49b520036329cf
Vito Farenga <vitofarenga@libero.it>
2022-06-28 17:09:41 +0200

    stringhe pushate nel dom

--- main.js
+++ main.js
@@ -2,35 +2,48 @@ const team = [
     {
        immagine : "wayne-barnett-founder-ceo.jpg",
        nome : "Wayne Barnett",
-       ruolo : "Founder & CEO"
+       ruolo : "Founder & CEO",
     },
     {
         immagine : "angela-caroll-chief-editor.jpg",
         nome : "Angela Carroll",
-        ruolo : "Chief Editor"
+        ruolo : "Chief Editor",
      },
      {
         immagine : "walter-gordon-office-manager.jpg",
         nome : "Walter Gordon",
-        ruolo : "Office Manager"
+        ruolo : "Office Manager",
      },
      {
         immagine : "angela-lopez-social-media-manager.jpg",
         nome : "Angela Lpoez",
-        ruolo : "Social Media Manager"
+        ruolo : "Social Media Manager",
      },
      {
-         immagine : "scott-estrada-developer.jpg",
+         immagine : "scott-estrada-developer.jpg", 
          nome : "Scott Estrada",
-         ruolo : "Developer"
+         ruolo : "Developer",
       },
       {
          immagine : "barbara-ramos-graphic-designer.jpg",
          nome : "Barbara Ramos",
-         ruolo : "Graphic Designer"
+         ruolo : "Graphic Designer",
       },
 ];
 
+const content = document.getElementById('carte')
+
 for(i = 0; i < team.length; i++){
+    content.innerHTML += `<div>
+    <img src="${team[i].immagine}">
+    <span>${team[i].nome}</span>
+    <span>${team[i].ruolo}</span>
+    
+    
+    </div>`;
+    
+    
+    
+    
     console.log(team[i]);
 };
